590f0ad44d368548a15ed27aaac876097e3b0e73
Dmitry Tumashev <tuma98@gmail.com>
2021-02-18 10:01:47 +0700

    A lot of new translated strings and algo changes

--- content.js
+++ content.js
@@ -1,34 +1,161 @@
 var strings = {
-    //Общее
+    //#region Common
+    "Saving...": "Сохранение...",
+    "Loading...": "Загрузка...",
+    "New": "Новое",
+    "Create": "Создать",
+    "Remove": "Удалить",
+    "Select": "Выбрать",
+    "Open": "Открыть",
+    "Clear": "Очистить",
+    "Cancel": "Отменить",
+    "Upload": "Загрузить",
+    "Link": "Ссылка",
+    "Submit": "Отправить",
+    //#endregion
 
-    //Side toolbar
+
+    //#region Login
+    "Log in": "Войти",
+
+    "Continue with Google": "Продолжить с Google",
+    "Continue with Apple": "Продолжить с Apple",
+
+    "Continue with email": "Продолжить, используя почту",
+    "Create new account": "Создать новый аккаунт",
+    "Continue with password": "Продолжить с паролем",
+
+    "Forgot password?": "Забыли пароль?",
+    "You can also ": "Вы также можете ",
+    "continue with SAML SSO": "продолжить с SAML SSO",
+
+    "There is no existing account under this email address. We just sent you a temporary sign up code.": "На этот адрес электронной почты не зарегистирован ни один аккаунт. Мы только что отослали вам временный код для регистрации.",
+    "Please check your inbox.": "Пожалуйста, проверьте свою папку входящих писем.",
+    "Sign up code": "Код регистрации",
+
+    "Password": "Пароль",
+    "Incorrect password.": "Неверный пароль.",
+    //#endregion
+
+    //#region Settings
+
+    //Sections
+    "My account": "Мой аккаунт",
+    "My notifications": "Мои уведомления",
+    "My connected apps": "Подключенные приложения",
+    "Language & region": "Язык и регион",
+    "Appearance": "Внешний вид",
+    "Earn credit": "Заработать бонусы",
+    "Workspace": "Рабочее пространство",
+    "Members": "Участники",
+    "Upgrade": "Улучшить тариф",
+    "Plans": "Тарифы",
+    "Billing": "Оплата",
+    "Security & identity": "Безопасность и идентификация",
+
+    //#endregion
+
+    //#region Side toolbar
     "Quick Find": "Быстрый поиск",
     "Settings": "Настройки",
+    "Settings & Members": "Настройки и участники",
     "All Updates": "Все обновления",
 
     "Shared": "Общие",
     "Private": "Личные",
+    "Favorites": "Избранное",
+    "Trash": "Корзина",
+    "New page": "Новая страница",
 
     "You are currently a guest. To see all the workspace pages, ask an admin to upgrade you to a member.": "Сейчас вы являетесь гостем. Чтобы просмотреть все страницы рабочего пространства, попросите администратора повысить вас до участника.",
     "Create a workspace": "Создать рабочее пространство",
-    //Top toolbar
+    //#endregion
+
+    //#region Top toolbar
     "Share": "Поделиться",
     "Updates": "Обновления",
     "Favorite": "В избранное",
     "Favorited": "В избранном",
-    //Menu
-    "Database lock": "Заблокировать базу данных",
+    //#endregion
+
+    //#region Menu
+    "Database lock": "Блокировка базы данных",
     "Add to Favorites": "Добавить в избранное",
+    "Remove from Favorites": "Убрать из избранного",
     "Copy link": "Скопировать ссылку",
-    "Open in Windows app": "Открыть в приложении для Windows",
+    "Open in Windows app": "Открыть в приложении",
     "Undo": "Отменить",
-    "Page history": "История страницы",
-    "Personal Pro": "Персональный Про",
+    "Page history": "Изменения",
+    "Show deleted pages": "Удалённые страницы",
     "Export": "Экспортировать",
     "PDF, HTML, Markdown": "PDF, HTML, Markdown",
     "Merge with CSV": "Слить с CSV",
+    "Rename": "Переименовать",
+    "Edit property": "Изменить свойство",
+    "Move to": "Переместить",
+    "Move to ": "Переместить в ",
+    "Private pages": "Личные страницы",
+    "Comment": "Прокомментировать",
 
-    //Database
+    "Style": "Стиль",
+    "Default": "Обычный",
+    "Serif": "Засечки",
+    "Mono": "Моно",
+    "Small text": "Маленький текст",
+    "Full width": "На всю ширину",
+    "Duplicate": "Создать копию",
+    "Import": "Импорт",
+    "Customize page": "Изменить страницу",
+    "Lock page": "Заблокировать страницу",
+    "Unlock page": "Разблокировать страницу",
+    //#endregion
+
+    //#region Share
+    "Share to web": "Открытый доступ",
+    "Publish and share link with anyone": "Открывает доступ для всех по ссылке",
+
+    "Add people, groups, or emails": "Люди, группы или адреса почты",
+    "Add people": "Добавить людей",
+
+    "Allow editing": "Разрешить редактирование",
+    "Allow comments": "Разрешить комментирование",
+    "Allow duplicate as template": "Разрешить копирование в качестве шаблона",
+    "Search engine indexing": "Индексирование поисковыми движками",
+
+    "Based on ": "На основе ",
+
+    "Full access": "Полный доступ",
+    "Can edit and share with others.": "Может редактировать и делиться с другими",
+    "Can edit": "Может редактировать",
+    "Can edit, but not share with others.": "Может редактировать, но не делиться с другими",
+    "Can comment": "Может комментировать",
+    "Can view and comment, but not edit.": "Может просматрировать и комментировать, но не редактировать",
+    "Can view": "Может просматировать",
+    "Cannot edit or share with others.": "Не может редактировать или делиться с другими",
+    "You’ll override permissions from the parent page if you change this role.": "Вы переопределите права доступа, полученные от родительской страницы, если измените эту роль.",
+    //#endregion
+
+    //#region Updates
+    "Follow this page": "Следить за изменениями страницы",
+    "Connect Slack channel": "Подключить канал Slack",
+
+    " in ": " в ",
+    " updated permission for ": " обновил(а) разрешения для",
+    " edited ": " изменил(а) ",
+    " commented in ": " прокомментировал(а) ",
+    " locked ": " заблокировал(а) ",
+    " unlocked ": " разблокировал(а) ",
+    " created ": " создал(а) ",
+    " deleted ": " удалил(а) ",
+    " edited view ": " изменил(а) представление ",
+    " created view ": " создал(а) представление ",
+    " deleted view ": " удалил(а) представление ",
+    " created property ": " создал(а) свойство ",
+    " edited property ": " изменил(а) свойство ",
+    " deleted property ": " удалил(а) свойство ",
+    //#endregion
+
+    //#region Database
     "Properties": "Свойства",
     "Group by ": "Группировать по ",
     "Filter": "Фильтр",
@@ -36,8 +163,7 @@ var strings = {
     "Hide": "Скрыть",
     "Delete": "Удалить",
     "Search": "Поиск",
-    "New": "Новая запись",
-    "Copy link to view": "Скопировать ссылку на представление",
+    "Copy link to view": "Ссылка на представление",
     "Templates": "Шаблоны",
     "Group by": "Изменить группировку",
     "Hidden columns": "Скрытые колонки",
@@ -45,47 +171,294 @@ var strings = {
     "Add a view": "Добавить представление",
     "Add a group": "Добавить группу",
 
-    //Page
-    "Open as page": "Открыть как страницу",
+    //#region Views
+    "Table": "Таблица",
+    "Table with types to store and view any kind of structured data": "Таблица с типами для хранения и отображения любых структурированных данных",
+    "Board": "Доска",
+    "Kanban board, great for project planning and bug tracking": "Канбан доска, отлично подходит для построения плана проекта и отслеживания багов",
+    "Timeline": "Временная шкала",
+    "A timeline view, great for project scheduling and planning": "Временная шкала, отлично подходит для создания расписания и плана проекта",
+    "Calendar": "Календарь",
+    "Month view, for event planning and scheduling": "Представление по месяцам для планирования событий и построения расписагия",
+    "List": "Список",
+    "A simplified page view, great for bookmarks and notes": "Упрощенное представление страниц, отлично подходит для закладок и заметок",
+    "Gallery": "Галерея",
+    "Grid of cards, use for mood boards, index cards, and recipes": "Сетка карточек, подходит для мудбордов, рецептов и подобного",
+    "View name": "Название представления",
+    //#endregion
 
-    //Editor
-    //Blocks
+    //#region Properties
+    "Properties for": "Свойства представления",
+
+    "Card preview": "Предпросмотр карточки",
+    "Page cover": "Обложка страницы",
+    "Page content": "Содержимое страницы",
+
+    "Card size": "Размер карточки",
+    "Small": "Маленький",
+    "Medium": "Средний",
+    "Large": "Большой",
+
+    "Add a property": "Новое свойство",
+    //#endregion
+
+    //#region Filters
+    "Filters for": "Фильтры для",
+    "Add a filter": "Добавить фильтр",
+    "Add a filter group": "Добавить группу фильтров",
+    "A group to nest more filters": "Группа для совмещения большего числа фильтров",
+    "Turn into group": "Превратить в группу",
+    "Turn into filter": "Превратить в фильтр",
+    "Wrap in group": "Обернуть в группу",
+    "Create a filter group around this": "Создать группу фильтров для этой группы",
+
+    "Where": "Где",
+    "And": "И",
+    "Or": "Или",
+
+    "Contains": "Содержит",
+    "Does not contain": "Не содержит",
+    "Is empty": "Пустое",
+    "Is not empty": "Не пустое",
+
+    "Pick a person": "Выберите человека",
+    "Me": "Меня",
+    //#endregion
+
+    //#region Sort
+    "Sort for": "Сортировка для",
+    "Ascending": "По возрастанию",
+    "Descending": "По убыванию",
+    "Add a sort": "Добавить сортировку",
+    //#endregion
+
+    //#region 
+    "Templates for": "Шаблоны для",
+    "New template": "Новый шаблон",
+    "Empty page": "Пустая страница",
+    //#endregion
+
+    //#endregion
+
+    //#region Page
+    "Open as page": "Открыть как страницу",
+    "Add to": "Добавить в",
+
+    "Add description": "Добавить описание",
+    "Hide description": "Спрятать описание",
+
+    "Add cover": "Добавить обложку",
+    "Change cover": "Изменить обложку",
+    "Reposition": "Переместить",
+    "Save position": "Сохранить позицию",
+
+    //Cover dialog
+    "Choose an image": "Выберите изображение",
+    "Images wider than 1500 pixels work best.": "Изображения шире 1500 пикселей выглядят лучше всего.",
+    "Works with any image from the web.": "Работает с любым изображением из интернета.",
+
+    //Icon
+    "Add icon": "Добавить иконку",
+    "Upload an image": "Загрузить изображение",
+    "Random": "Случайно",
+    "Recommended size is 280 × 280 pixels": "Рекомендованный размер - 280 х 280 пикселей",
+    "Upload an image": "Загрузить",
+
+    //Comments
+    "Add comment": "Добавить комментарий",
+    "Send": "Отправить",
+    "Resolve": "Решено",
+    "Edit comment": "Изменить комментарий",
+    "Delete comment": "Удалить комментарий",
+    //#endregion
+
+    //#region Editor
+
+    //#region Blocks
+    "Basic blocks": "Базовые блоки",
+    "Inline": "Встроенные блоки",
+    "Media": "Медиа",
+
     "Text": "Текст",
-    "Just start writing with plain text.": "Начните печатать обычным текстом",
+    "Just start writing with plain text.": "Начните печатать обычным текстом.",
 
     "Page": "Страница",
-    "Embed a sub-page inside this page.": "Вставьте подстраницу в эту страницу",
+    "Embed a sub-page inside this page.": "Вставьте подстраницу в эту страницу.",
 
     "To-do list": "Список дел",
-    "Track tasks with a to-do list.": "Отслеживайте задачи с помощью списка дел",
+    "Track tasks with a to-do list.": "Отслеживайте задачи с помощью списка дел.",
 
     "Heading 1": "Заголовок 1",
-    "Big section heading.": "Большой заголовок раздела",
+    "Big section heading.": "Большой заголовок раздела.",
 
     "Heading 2": "Заголовок 2",
-    "Medium section heading.": "Средний заголовок раздела",
+    "Medium section heading.": "Средний заголовок раздела.",
 
     "Heading 3": "Заголовок 3",
-    "Small section heading.": "Маленький заголовок раздела",
+    "Small section heading.": "Маленький заголовок раздела.",
+
+    "Bulleted list": "Маркированный список",
+    "Create a simple bulleted list.": "Создаёт простой маркированный список.",
+
+    "Numbered list": "Нумерованный список",
+    "Create a list with numbering.": "Создаёт список с нумерацией.",
+
+    "Toggle list": "Раскрывающийся список",
+    "Toggles can hide and show content inside.": "Раскрывающиеся списки могут прятать и показывать содержимое.",
+
+    "Quote": "Цитата",
+    "Capture a quote.": "Процитируй кого-нибудь.",
+
+    "Divider": "Разделитель",
+    "Visually divide blocks.": "Визуально разделяет блоки.",
+
+    "Link to page": "Ссылка на страницу",
+    "Link to an existing page.": "Ссылка на существующую страницу.",
+
+    "Callout": "Выделение",
+    "Make writing stand out.": "Делает написанное очень заметным.",
+
+    "Mention a person": "Упомянуть участника",
+    "Ping someone so they get a notification.": "Тыкает участника, чтобы он получил уведомление.",
+
+    "Mention a page": "Упомянуть страницу",
+    "Mention a page and place in text.": "Вставляет упоминание страницы в текст.",
+
+    "Date or reminder": "Дата или напоминание",
+    "Insert a date or reminder in text.": "Вставляет дату или напоминание в текст.",
+
+    "Emoji": "Эмодзи",
+    "Search for an emoji to place in text.": "Поиск и вставка эмодзи в текст.",
+
+    "Inline equation": "Встроенное уравнение",
+    "Insert mathematical symbols in text.": "Вставляет математические символы в текст.",
+
+    "Image": "Изображение",
+    "Upload or embed with a link.": "Загрузить или встроить со ссылкой.",
+
+    "Web bookmark": "Веб-закладка",
+    "Save a link as a visual bookmark.": "Сохранить ссылку как визуальную закладку.",
+
+    "Video": "Видео",
+    "Embed from YouTube, Vimeo...": "Встроить с YouTube, Vimeo...",
+
+    "Audio": "Аудио",
+    "Embed from SoundCloud, Spotify...": "Встроить с SoundCloud, Spotify...",
+
+    "Code": "Код",
+    "Capture a code snippet.": "Вставить фрагмент кода.",
+
+    "File": "Файл",
+    "Upload or embed with a link.": "Загрузить или встроить со ссылкой.",
+    //#endregion
+
+    //#region Date-Time selector
+    "Remind": "Напомнить",
+
+    "None": "Отключено",
+    "At time of event": "В момент начала события",
+    "5 minutes before": "За 5 минут до начала",
+    "10 minutes before": "За 10 минут до начала",
+    "15 minutes before": "За 15 минут до начала",
+    "30 minutes before": "За 30 минут до начала",
+    "1 hour before": "За 1 час до начала",
+    "2 hours before": "За 2 часа до начала",
+    "On day of event (9:00 AM)": "В день события (9:00)",
+    "1 day before (9:00 AM)": "За 1 день до события (9:00)",
+    "2 days before (9:00 AM)": "За 2 дня до события (9:00)",
+    "1 week before (9:00 AM)": "За 1 неделю до события (9:00)",
+
+    "End date": "Дата окончания",
+    "Include time": "Добавить время",
+    "Date format & timezone": "Формат даты и часовой пояс",
+
+    "Date format": "Формат даты",
+    "Full date": "Полная дата",
+    "Month/Day/Year": "Месяц/День/Год",
+    "Day/Month/Year": "День/Месяц/Год",
+    "Year/Month/Day": "Год/Месяц/День",
+    "Relative": "Относительный",
+
+    "Time format": "Формат времени",
+    "12 hour": "12 часов",
+    "24 hour": "24 часа",
+
+    "Timezone": "Часовой пояс",
+    //#endregion
+
+    //#endregion
+
+    //#Search
+    "Recent pages": "Недавние страницы",
+    "Open in a new tab": "Открыть в новой вкладке",
+    //#Tooltips
+    "Pin this page in your sidebar": "Закрепить эту страницу в боковом меню"
 }
 
+var placeholders = {
+    //#region Page
+    "Add a comment…": "Добавить комментарий...",
+    "Type '/' for commands": "Введите '/' для открытия панели команд",
+    "Untitled": "Без названия",
+    "Press Enter to continue with an empty page, or pick a template (↑↓ to select)": "Нажмите Enter, чтобы продолжить с пустой страницей, или выберите шаблон (↑↓ для выбора)",
+    "Empty": "Пусто",
+    "Add a description…": "Добавьте описание...",
+    //#endregion
+
+    "Type to search...": "Введите запрос",
+    "Enter your email address...": "Введите вашу почту",
+    "Enter your password...": "Введите ваш пароль...",
+    "Paste login code": "Вставьте код регистрации",
+
+    "Add to another page...": "Добавить на другую страницу...",
+    "Filter actions…": "Отфильтровать действия...",
+
+    "Paste an image link…": "Вставьте ссылку на изображение...",
+    "Search for an image…": "Поиск по изображению...",
+    "Search for a person…": "Поиск по участнику...",
+    "Type something…": "Напишите что-нибудь...",
+    "Filter...": "Отфильтровать",
+    "Search for a page, or create a new one…": "Найдите страницу или создайте новую...",
+    "Add new page in…": "Добавить страницу в...",
+}
+
+var stringParts = {
+    "Today at": "Сегодня в"
+}
+
+
 function translateNode(node) {
+    if (node.nodeType === 1 && node.hasAttribute("placeholder")) {
+        translatePlaceholder(node);
+        return;
+    }
+
     if (node.innerHTML in strings) {
         var translated = strings[node.innerHTML];
         console.log(`[innerHTML] translating node, old value: "${node.innerHTML}", new value: "${translated}"`)
         node.innerHTML = translated;
-    }
-
-
+    } 
+    //else пока нафиг ломает кучу элементов
+    // else {
+    //     //console.log(`Searching stringParts for innerHTML: ${node.innerHTML}`);
+    //     Object.keys(stringParts).forEach(function(key) {
+    //         //
+    //         //logic();
+    //         //console.log(`Key: ${key}`);
+    //         if (node.innerHTML != null && node.innerHTML.includes(key)) {
+    //             let translated = stringParts[key];
+    //             console.log(`[stringParts, innerHTML] translating node, old value: "${node.innerHTML}", new value: "${translated}"`)
+    //             node.innerHTML = node.innerHTML.replace(key, translated);
+    //         }
+    //       });
+    // }
 
     if (node.data in strings) {
         if (node.data != null) console.log(`node data: ${node.data}`);
         var translated = strings[node.data];
         console.log(`[data] translating node, old value: "${node.data}", new value: "${translated}"`)
-        node.data = strings[node.data]
+        node.data = translated;
     };
-
-
 
     if (node.childNodes != null) {
         var nodes = node.childNodes;
@@ -93,6 +466,16 @@ function translateNode(node) {
             translateNode(nodes[i]);
         }
     }
+}
+
+function translatePlaceholder(node) {
+    let placeholderValue = node.getAttribute("placeholder");
+        console.log(`node placeholder: ${placeholderValue}`);
+        if (placeholderValue in placeholders) {
+            var translated = placeholders[placeholderValue];
+            console.log(`[placeholder] translating node, old value: "${placeholderValue}", new value: "${translated}"`)
+            node.setAttribute("placeholder", translated);
+        }
 }
 
 // Select the node that will be observed for mutations
@@ -105,9 +488,21 @@ const config = { attributes: true, childList: true, subtree: true };
 const callback = function (mutationsList, observer) {
 
     for (let mutation of mutationsList) {
-        for (let node of mutation.addedNodes) {
-            translateNode(node);
+        if (mutation.type == "attributes") {
+            if (mutation.attributeName = "placeholder") continue;
+            //console.log(`Mutated attribute: ${mutation.attributeName}`);
+            translatePlaceholder(mutation.target);
+        } 
+        console.log(`Mutated! Type: ${mutation.type}`);
+        if (mutation.type == "childList") {
+            for (let node of mutation.addedNodes) {
+                translateNode(node);
+            }
+        } 
+        if (mutation.type == "characterData") {
+            translateNode(mutation.target);
         }
+
     }
 };
 
@@ -115,6 +510,8 @@ const callback = function (mutationsList, observer) {
 const observer = new MutationObserver(callback, {
     childList: true, // наблюдать за непосредственными детьми
     subtree: true,// и более глубокими потомками
+    attributeFilter: ['placeholder'],
+    characterData: true,
 }
 );
 
